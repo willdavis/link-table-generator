6e37333a93c4b7ab1f8a01dcddca8c3d2c7d8ef5
Bill Davis <wdavis@responsys.com>
2012-11-05 15:47:14 -0800

    cleaned up debug output a little more

--- linkgen.rb
+++ linkgen.rb
@@ -8,16 +8,17 @@ puts "Scanning HTML files for <a href> tags..."
 ARGV.each do |arg|
 	File.open(arg, 'r') do |rfile|
 		while line = rfile.gets
-			match = line.match @regex
-			@matches.push([match[1],arg]) if match
+			matchdata = line.match @regex
+			@matches.push([matchdata[1],arg]) if match
 		end
 	end
 end
 
 @matches.each do |match|
-	puts "Extracting: #{match[0]} from: #{match[1]}"
+	puts "Found: #{match[0]} in: #{match[1]}"
 end
 
+puts "Creating link table..."
 File.open(@linkCSV, 'w') do |wfile|
 	#Format the column headers
 	wfile.puts("FILE_NAME,LINK_NAME,LINK_URL,LINK_CATEGORY")
@@ -28,4 +29,6 @@ File.open(@linkCSV, 'w') do |wfile|
 	end
 end
 
+puts "All done!"
+puts "link table generated at: #{@linkCSV}"
 puts "Total URL's extracted: #{@matches.count}"
