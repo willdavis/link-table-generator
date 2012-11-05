d7069747b831968fefa273a0578f65131446c0dc
Bill Davis <wdavis@responsys.com>
2012-11-05 14:21:19 -0800

    linktable file path will be the same as the ruby scripts

--- linkgen.rb
+++ linkgen.rb
@@ -5,9 +5,11 @@ puts "Parsing: #{ARGV[0]}"
 
 @regex = /<a href="(\S*)"/
 @matches = []
+@linkCSV = "#{File.dirname(__FILE__)}/linkTable.csv"
+puts @linkCSV
 
-File.open(ARGV[0], 'r') do |f|
-	while line = f.gets
+File.open(ARGV[0], 'r') do |rfile|
+	while line = rfile.gets
 		match = line.match @regex
 		@matches.push(match[1]) if match
 	end
@@ -16,3 +18,9 @@ end
 @matches.each do |match|
 	puts "Found: #{match}"
 end
+
+#File.open(@linkCSV, 'w') do |wfile|
+#	@matches.each do |match|
+#		wfile.puts(match)
+#	end
+#end
