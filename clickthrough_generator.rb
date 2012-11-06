8287ce56ee36f6e854ed727b5fe58ac86a107299
Bill Davis <wdavis@rmsfe642wdavis.us.responsys.com>
2012-11-06 14:56:11 -0800

    csv file is now read into a buffer

--- clickthrough_generator.rb
+++ clickthrough_generator.rb
@@ -1,7 +1,9 @@
 #!/usr/bin/ruby
 
 @creatives = Hash.new
+@csv_buffer = []
 
+# Open files passed via command line and read their contents into a buffer.
 ARGV.each do |arg|
 	File.open(arg, 'r') do |rfile|
 		case File.extname(arg)
@@ -10,14 +12,21 @@ ARGV.each do |arg|
 		when ".txt"
 			@creatives["#{rfile.path}"] = rfile.read
 		when ".csv"
-			
+			while line = rfile.gets
+				@csv_buffer.push(line)
+			end 
 		else
 			puts "File extension not recognized"
 		end
 	end
 end
 
+#DEBUG OUTPUT
+@csv_buffer.each do |item|
+	puts item
+end
+
 @creatives.each do |fname, content|
-	puts "#{fname} has content: #{content}"
+	puts "#{fname} content is:\n#{content}"
 end
 
